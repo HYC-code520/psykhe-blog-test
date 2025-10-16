2a4bcef64f399e39f5563c535521dcb8c3a29bfb
Butter Chen <butterchen@Butters-MacBook-Pro.local>
2025-10-16 13:46:08 -0400

    Remove TinaCMS from project

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/butterchen/Desktop/New-psykhe-website/psykhe-website/tina/__generated__/.cache/1760399066458', url: 'https://content.tinajs.io/1.6/content/local/github/main', token: 'local', queries,  });
-export default client;
-  
